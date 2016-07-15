5ade9995fc97034e7e8402cc8a37b679a918798d
Xavier Via <fernando.via@gmail.com>
2016-07-15 15:14:07 +0200

    Cleanup?

--- compose.js
+++
@@ -1,4 +0,0 @@
-export default function (...fs: Array<(x: any) => any>) {
-  return (x: any): any =>
-    fs.reduceRight((y, f) => f(y), x)
-}
--- example.js
+++ example.js
@@ -1,14 +1,6 @@
 import example from 'washington'
-import compose from './compose'
+import compose from './compose/example'
 
-example('compose', (check) => {
-  const increment = (x) => x + 1
-  const duplicate = (x) => x * 2
-  const add = (y) => (x) => x + y
-
-  const allOfTheAbove = compose(add(3), duplicate, increment)
-
-  check(allOfTheAbove(5), 15)
-})
+example('compose', compose)
 
 example.go()
